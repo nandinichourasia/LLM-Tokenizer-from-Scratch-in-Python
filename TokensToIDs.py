3ccb42ac2cfc248d76af94fa91ee1198e381152e
nandinichourasia <nandinichourasia24@gmail.com>
2025-05-22 02:14:37 +0530

    Create TokensToIDs.py

---
+++ TokensToIDs.py
@@ -0,0 +1,17 @@
+with open("the-verdict.txt" , "r" , encoding = "utf-8") as f:
+    raw_text = f.read()
+import re
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)' , raw_text)
+preprocessed = [item for item in preprocessed if item.strip()]
+
+# preprocessed is the list of tokens now ,
+# ✅STEP2:
+# ✅Converting tokens into token IDs :  
+all_words = sorted(set(preprocessed))
+print(len(all_words))
+# Creating a vocabulary of all tokens and associating unique ID with it
+vocab = {token:integer for integer,token in enumerate(all_words)}
+for i,item in enumerate(vocab.items()):
+    print(item)
+    if i>=50:
+        break
